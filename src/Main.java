0eb2b9b3fb51f82d3df4bec7edfb9aea6902cedf
Jan <wittmannjan1984@gmail.com>
2022-11-06 16:57:13 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        double Asterix = 3;
+        double Obelix = Asterix / 2;
+        double Zeit = Asterix + Obelix;
+        double Elixir = 5;
+
+        boolean Superelixir = (Elixir <= Asterix + Obelix );
+
+        System.out.println(Asterix);
+        System.out.println(Obelix);
+        System.out.println(Zeit);
+        System.out.println(Elixir);
+        System.out.println(Superelixir);
+    }
+}
